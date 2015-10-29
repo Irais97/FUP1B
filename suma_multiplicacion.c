6c343951fd1647fe32cd749fbf5c62adc0a23060
Irais97 <huelitlirais@gmail.com>
2015-10-28 20:43:10 -0600

    irais

---
+++ suma_multiplicacion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int numero;
+int i=1;
+int a=0;
+int main()
+{
+	printf("programa para sumar\n");
+	printf("ingresa un numero\n");
+	scanf("%d",&a);
+	scanf("%d",&i);
+		while (i<=0)
+		{
+	      a=a+i;
+          i=i+1;
+       }
+	      ("el %d", a);
+}
