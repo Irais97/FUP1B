87e706f7ce8f836fbc5450b7cb040b6244a3ab15
Irais97 <huelitlirais@gmail.com>
2015-11-01 22:38:09 -0600

    Irais

---
+++ promedio_10_numeros.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+/* calcular el promedio*/
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int j;
+int promedio;
+int calificasion;
+int main()
+{//inicio
+  printf("CALCULAR EL PROMEDIO\n");
+  printf("escribe 10 calificasiones\n");
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&a);
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&b);
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&c);
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&d);
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&e);
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&f);
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&g);
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&h);
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&i);
+  
+  printf("ingresa la calificasion\n");
+  scanf("%d",&j);
+  
+    promedio=a+b+c+d+e+f+g+h+i+j /10;
+    printf("el promedio de las 10 calificasiones es %d\n",promedio);
+}//fin
