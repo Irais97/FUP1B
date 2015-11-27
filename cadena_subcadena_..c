a412428dd19ac31c279d0bd2b441bc87955c39c4
Irais97 <huelitlirais@gmail.com>
2015-11-26 23:14:12 -0600

    cadena

---
+++ cadena_subcadena_..c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int a=0;
+int b=0;
+int c=0;
+int i;
+char cadena[50];
+char subca[50];
+int main()
+{
+	printf("programa de una cadena\n");
+	printf("Escribe una palabra\n");
+	scanf("%s", cadena);
+	while(cadena[50]!='\0')
+	b++;
+	
+		printf("escribe otra palabra\n");
+		scanf("%s",subca);
+		while(subca[a]!='\0')
+		a++;
+
+	if(b<=a)
+	for(i=0;i<=a;i++)
+	if(cadena[i]==subca[c] || subca[c]!='\0')
+	i++;
+	if(c==b)
+	
+		
+		
+	{
+		printf("\n palabra no existente %s", subca);
+	}
+	{
+    	printf("no puede ser subca de %s", subca, cadena);
+   } 
+	
+}
+
